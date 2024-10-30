db5909084395f350e2cf683b32e38d804b2095f3
Rahal Danthanarayana <rahalncm@gmail.com>
2024-10-29 23:06:46 -0400

    Building Toc Tac Toe game

--- data-structures/class-questions.py
+++ data-structures/class-questions.py
@@ -1,3 +1,6 @@
+from enum import Enum
+
+
 def find_max_profits(price_array):
     # this will be O(n^2) time complexity solution , space complexity 1 (because just two variables)
     max_profit = price_array[1] - price_array[0]
@@ -100,6 +103,198 @@ def find_value_using_brute_force(size, array, sum):
     return result
 
 
+# class syntax
+
+class Mark(Enum):
+    Friendly = 1
+    Opposition = -1
+
+
+def play_TicTacToe(opposition_play_x, opposition_play_y, board):
+    # program plays 1
+    # enemy plays -1
+
+    board[opposition_play_x][opposition_play_y] = Mark.Opposition.value
+    print('board: ', board)
+    cell_count = 0
+    highest_win_points = 0
+    highest_def_points = 0
+    def_points, win_points = 0, 0
+    def_location, win_location = 0, 0
+    final_play_location = None
+    final_points = 0
+
+    for row_index, row in enumerate(board):
+        for cell_index, cell in enumerate(row):
+            print('cell: ', cell_count, ' value: ', cell)
+            # print win, def values where not yet played cells
+            if cell is not Mark.Friendly.value and cell is not Mark.Opposition.value:
+                cell_win_points, cell_def_points = 0, 0
+                cell_def_points += count_def_point(row_index, cell_index, board)
+                cell_win_points += count_win_points(row_index, cell_index, board)
+
+                # check is the point in diagonal
+                if row_index == cell_index:
+                    print('point is in main diagonal')
+                    def_points, win_points = check_upwards_diagonal(board)
+                    cell_win_points += win_points
+                    cell_def_points += def_points
+                if row_index == 0 and cell_index == (len(board) - 1) or (
+                        row_index == len(board) - 1 and cell_index == 0) or (row_index == 1 and cell_index == 1):
+                    print('point is diagonal: ', row_index, cell_index)
+                    def_points_upwards, win_points_upwards = check_downward_diagonal(board)
+                    cell_win_points += win_points_upwards
+                    cell_def_points += def_points_upwards
+                if cell_def_points > highest_def_points:
+                    highest_def_points = cell_def_points
+                    def_location = [row_index, cell_index]
+                if cell_win_points > highest_win_points:
+                    highest_win_points = cell_win_points
+                    win_location = [row_index, cell_index]
+                print('win points: ', cell_win_points, ' def points: ', cell_def_points)
+            cell_count += 1
+
+    if highest_win_points > highest_def_points:
+        print('going to play to win')
+        final_play_location = win_location
+        final_points = highest_win_points
+    else:
+        print('going to play defensive')
+        final_play_location = def_location
+        final_points = highest_def_points
+
+    return final_play_location, final_points
+
+
+def count_win_points(x, y, board):
+    row = board[x]
+    points = 0
+    i = len(board) - 1
+
+    if row.count(Mark.Opposition.value) == 0:
+        # means enemy have no pieces in the row
+        points += 1
+
+    # check urgent to win the row
+    if row.count(Mark.Friendly.value) == 2:
+        points += 1
+
+    column_found = False  # makes True if enemy mark found
+    friendly_count = 0
+    while not column_found and i >= 0:
+        if board[i][y] == Mark.Opposition.value:
+            column_found = True
+        if board[i][y] == Mark.Friendly.value:
+            friendly_count += 1
+        i -= 1
+
+    if not column_found:
+        # no enemy presence in the column
+        points += 1
+    # check if there is urgent win for the column
+    if friendly_count == 2:
+        points += 1
+
+    return points
+
+
+def count_def_point(x, y, board):
+    points = 0
+    row = board[x]
+    # row
+    if row.count(Mark.Opposition.value) > 0:
+        points += 1
+    # in urgent matter
+    if row.count(Mark.Friendly.value) == 2:
+        points += 1
+
+    # column
+    i = len(board) - 1
+    column_found = False
+    opposition_count = 0
+    while not column_found and i >= 0:
+        if board[i][y] == Mark.Opposition.value:
+            points += 1
+        if board[i][y] == Mark.Opposition.value:
+            opposition_count += 1
+        i -= 1
+    if opposition_count == 2:
+        points += 1
+    return points
+
+
+def check_diagonal_points(board):
+    opposition_count = 0
+    friendly_count = 0
+    down_opposition_count = 0
+    down_friendly_count = 0
+    points = 0
+    def_points = 0
+    # right to left diagonal, upwards
+    def_points, points = check_upwards_diagonal(board)
+
+    down_def_points, down_win_points = check_downward_diagonal(board)
+
+    return (points + down_win_points), (def_points + down_win_points)
+
+
+def check_downward_diagonal(board):
+    def_points, down_friendly_count, down_opposition_count, points = 0, 0, 0, 0
+    # right to left diagonal, downwards
+    i = 0
+    j = len(board[0]) - 1
+    while i < len(board) and j >= 0:
+        if board[i][j] == Mark.Opposition.value:
+            down_opposition_count += 1
+        if board[i][j] == Mark.Friendly.value:
+            down_friendly_count += 1
+        i += 1
+        j -= 1
+    if down_opposition_count == 0:
+        points += 1
+    if down_opposition_count > 0:
+        def_points += 1
+    # Urgent scenario to def
+    if down_opposition_count == 2:
+        def_points += 1
+    # URGENT scenario
+    if down_friendly_count == 2:
+        points += 1
+    return def_points, points
+
+
+def check_upwards_diagonal(board):
+    def_points, friendly_count, opposition_count, points = 0, 0, 0, 0
+    i = len(board) - 1
+    j = len(board[0]) - 1
+    while i >= 0 and j >= 0:
+        if board[i][j] == Mark.Opposition.value:
+            opposition_count += 1
+        if board[i][j] == Mark.Friendly.value:
+            friendly_count += 1
+        i -= 1
+        j -= 1
+    if opposition_count == 0:
+        points += 1
+    if opposition_count > 0:
+        def_points += 1
+    # Urgent scenario to def
+    if opposition_count == 2:
+        def_points += 1
+    # URGENT scenario
+    if friendly_count == 2:
+        points += 1
+    return def_points, points
+
+
+def count_win_point_diagonal(x, y, board):
+    # check if x,y belong to diagonal. if not return 0 for both
+    if x == y or (x == 0 and y == (len(board) - 1)) or (x == len(board) - 1 and y == 0):
+        return 1
+    else:
+        return 0
+
+
 if __name__ == '__main__':
     # print(find_max_profits([35, 45, 6, 10, 22, 11, 80]))
     # print(find_id_peak([1, 0, 0, 0, 0, 0, 1]))
@@ -115,4 +310,7 @@ if __name__ == '__main__':
     rows, cols = (8, 8)
     arr = [[0] * cols] * rows
     # print(place_queen_in_chessboard(arr))
-    print(find_value_using_brute_force(8, [1,4,8,9,10,44], sum=56))
+    # print(find_value_using_brute_force(8, [1,4,8,9,10,44], sum=56))
+    board = [[0] * 3 for i in range(3)]
+    location, points = play_TicTacToe(1, 1, board)
+    print('computer plays on ', location, ' for ', points, ' points')
